3914c89e511873a59735fa47216d1cf0f83037ab
Swarnim Jain <90375441+swarnim992@users.noreply.github.com>
2026-02-06 15:05:25 +0530

    Update experience entries with new roles and dates

--- lib/data/experience.dart
+++ lib/data/experience.dart
@@ -2,13 +2,23 @@ import 'package:Swarnim_Jain/models/models.dart';
 
 List<Experience> Experiences = [
   Experience(
-    role: 'Software Development \nEngineer',
-    companyName: 'Decimal Point Analytics',
-    startTime: 'Mar 2024',
+    role: 'AI Engineer',
+    companyName: 'Negenux Solutions',
+    startTime: 'Feb 2026',
     endTime: 'Present',
     workType: 'On Site',
     duration: '1 Year',
     start: true,
+    end: false,
+  ),
+  Experience(
+    role: 'Software Development \nEngineer',
+    companyName: 'Decimal Point Analytics',
+    startTime: 'Mar 2024',
+    endTime: 'Jan 2026',
+    workType: 'On Site',
+    duration: '2 Year',
+    start: false,
     end: true,
   ),
 ];
